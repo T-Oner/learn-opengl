2b46656da9090f93ba055bd42f90d84e06d93daa
T-Oner <1@fxcdev.com>
2022-06-18 15:40:40 +0800

    [add] add shader

--- src/1.getting_started/2.hello_triangle/hello_triangle.cpp
+++ src/1.getting_started/2.hello_triangle/hello_triangle.cpp
@@ -2,6 +2,43 @@
 // Created by 冯旭超 on 2022/6/17.
 //
 
+#include "glad/glad.h"
+#include <iostream>
+
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+
 int main() {
+    // 顶点坐标数据
+    float vertices[] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f, -0.5f, 0.0f,
+    };
+
+    // 定义顶点缓冲对象
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // 创建着色器并编译
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX"""
+    }
+
     return 0;
 }
